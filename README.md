ff10ebf5762c97d523caf842629be999576a5ac4
maritvandijk <mlvandijk@gmail.com>
2017-07-06 13:37:08 +0200

    Updated description for FuzzyDucky to FizzBuzz

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # tdd-practice
 A project to store coding katas used for TDD practice.
+
 This project contains several well-known coding katas, as well as possible solutions.
 
---
+++ src/test/java/fizzbuzz/FizzBuzzShould.java
@@ -0,0 +1,23 @@
+package fizzbuzz;
+
+public class FizzBuzzShould {
+	/**
+	 * The theatric warm-up game 'Fizz Buzz' is played by the players forming a circle and in turn simply counting up, except:
+	 * - if a number is divisable by 3 (e.g. 6) -or contains a 3- (e.g. 13)- in stead the player says "Fizz"
+	 * - if a number is divisable by 5 (e.g. 20) -or contains a 5 (e.g. 25)- in stead the player says "Buzz"
+	 * - if both rules above apply, the player says "FizzBuzz"
+	 *
+	 * A player is out if he fails to say the correct number or fails to substitute a candidate.
+	 * (Of course, a player is also out if his answer starts with "Dizz"...)
+	 *
+	 * Implement the perfect player that, given a number, correctly returns the number, "Fizz", "Buzz" or "FizzBuzz" respectively
+	 *
+	 * Examples
+	 * 13 -> "Fizz"
+	 * 14 -> "Buzz"
+	 * 15 -> "Fuzzy"
+	 * 16 -> 16
+	 *
+	 * 27 -> "FizzBuzz"
+	 */
+}
