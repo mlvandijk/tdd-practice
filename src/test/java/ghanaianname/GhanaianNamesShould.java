965af1b23afe2a6aac2f7981a387965f35e23604
maritvandijk <mlvandijk@gmail.com>
2017-07-30 16:41:34 +0200

    Added new assignment Ghanaian Name

---
+++ src/test/java/ghanaianname/GhanaianNamesShould.java
@@ -0,0 +1,20 @@
+package ghanaianname;
+
+/*
+* In Ghana, you get a name based on the day of the week you were born (see table below)
+* Create a program that takes a date and sex from the user, and returns their Ghanaian Name
+*
+* Week day  | Female    | Male
+* SUNDAY 	| Akosua    | Kwasi
+* MONDAY    | Adjoa     | Kodjo
+* TUESDAY   | Abenaa    | Kwabena
+* WEDNESDAY | Akua      | Kwaku
+* THURSDAY  | Yaa       | Yaw
+* FRIDAY    | Afia      | Kofi
+* SATURDAY  | Amma      | Kwame
+*
+* Source (includes pronunciation): https://thisworldmusic.com/african-day-name-generator/
+*/
+
+public class GhanaianNamesShould {
+}
