c4743fa47cec6a375ea27aaef96ad60e21aebe43
maritvandijk <mlvandijk@gmail.com>
2017-08-01 03:04:58 +0200

    Refactor creation of date from String

--- src/main/java/ghanaianname/GhanaianNames.java
+++ src/main/java/ghanaianname/GhanaianNames.java
@@ -1,15 +1,27 @@
 package ghanaianname;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GhanaianNames {
 
-    public static String getDayOfWeek(LocalDate date) {
-        return date.getDayOfWeek().name();
+    public static String getDayOfWeek(LocalDate dateInput) {
+        return dateInput.getDayOfWeek().name();
     }
 
-    public static LocalDate getLocalDate(String date) {
-        String[] tokens = date.split("-");
-        return LocalDate.of(Integer.valueOf(tokens[0]), Integer.valueOf(tokens[1]), Integer.valueOf(tokens[2]));
+    public static LocalDate getLocalDate(String dateInput) {
+        List<Integer> tokens = transformStringToInts(dateInput);
+        return LocalDate.of(tokens.get(0), tokens.get(1), tokens.get(2));
+    }
+
+    private static List<Integer> transformStringToInts(String date) {
+        String[] dateAsStrings = date.split("-");
+        List<Integer> dateAsInts = new ArrayList<>();
+        for (int i = 0; i < 3; i++){
+            int integer = Integer.valueOf(dateAsStrings[i]);
+            dateAsInts.add(integer);
+        }
+        return dateAsInts;
     }
 }
