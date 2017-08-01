babf4a91bb20388792ee4c3b5ff9519cb7171000
maritvandijk <mlvandijk@gmail.com>
2017-08-01 02:35:10 +0200

    Passed test to get date from String(GREEN)

--- src/main/java/ghanaianname/GhanaianNames.java
+++ src/main/java/ghanaianname/GhanaianNames.java
@@ -7,4 +7,9 @@ public class GhanaianNames {
     public static String getDayOfWeek(LocalDate date) {
         return date.getDayOfWeek().name();
     }
+
+    public static LocalDate getLocalDate(String date) {
+        String[] tokens = date.split("-");
+        return LocalDate.of(Integer.valueOf(tokens[0]), Integer.valueOf(tokens[1]), Integer.valueOf(tokens[2]));
+    }
 }
